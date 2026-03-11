5f063ae4fbfb6e73dfb9adc1784bef07c68337fe
Alamgir <atamoori@roadpost.com>
2026-03-11 17:31:45 +0500

    Fix ProductScreen hook dependency warning

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -35,8 +35,7 @@ const OrderScreen = ({ match, history }) => {
   const userLogin = useSelector((state) => state.userLogin)
   const { userInfo } = userLogin
 
-  if (!loading) {
-    //   Calculate prices
+  if (!loading && order) {
     const addDecimals = (num) => {
       return (Math.round(num * 100) / 100).toFixed(2)
     }
@@ -53,13 +52,16 @@ const OrderScreen = ({ match, history }) => {
 
     const addPayPalScript = async () => {
       const { data: clientId } = await axios.get('/api/config/paypal')
+
       const script = document.createElement('script')
       script.type = 'text/javascript'
       script.src = `https://www.paypal.com/sdk/js?client-id=${clientId}`
       script.async = true
+
       script.onload = () => {
         setSdkReady(true)
       }
+
       document.body.appendChild(script)
     }
 
@@ -67,17 +69,18 @@ const OrderScreen = ({ match, history }) => {
       dispatch({ type: ORDER_PAY_RESET })
       dispatch({ type: ORDER_DELIVER_RESET })
       dispatch(getOrderDetails(orderId))
-    } else if (!order.isPaid) {
+    } 
+    else if (!order.isPaid) {
       if (!window.paypal) {
         addPayPalScript()
       } else {
         setSdkReady(true)
       }
     }
-  }, [dispatch, orderId, successPay, successDeliver, order])
+
+  }, [dispatch, orderId, successPay, successDeliver, order, userInfo, history])
 
   const successPaymentHandler = (paymentResult) => {
-    console.log(paymentResult)
     dispatch(payOrder(orderId, paymentResult))
   }
 
@@ -92,55 +95,71 @@ const OrderScreen = ({ match, history }) => {
   ) : (
     <>
       <h1>Order {order._id}</h1>
+
       <Row>
         <Col md={8}>
           <ListGroup variant='flush'>
+
             <ListGroup.Item>
               <h2>Shipping</h2>
               <p>
-                <strong>Name: </strong> {order.user.name}
+                <strong>Name:</strong> {order.user.name}
               </p>
+
               <p>
-                <strong>Email: </strong>{' '}
-                <a href={`mailto:${order.user.email}`}>{order.user.email}</a>
+                <strong>Email:</strong>{' '}
+                <a href={`mailto:${order.user.email}`}>
+                  {order.user.email}
+                </a>
               </p>
+
               <p>
                 <strong>Address:</strong>
-                {order.shippingAddress.address}, {order.shippingAddress.city}{' '}
+                {order.shippingAddress.address},{' '}
+                {order.shippingAddress.city},{' '}
                 {order.shippingAddress.postalCode},{' '}
                 {order.shippingAddress.country}
               </p>
+
               {order.isDelivered ? (
                 <Message variant='success'>
                   Delivered on {order.deliveredAt}
                 </Message>
               ) : (
-                <Message variant='danger'>Not Delivered</Message>
+                <Message variant='danger'>
+                  Not Delivered
+                </Message>
               )}
             </ListGroup.Item>
 
             <ListGroup.Item>
               <h2>Payment Method</h2>
-              <p>
-                <strong>Method: </strong>
-                {order.paymentMethod}
-              </p>
+              <strong>Method:</strong> {order.paymentMethod}
+
               {order.isPaid ? (
-                <Message variant='success'>Paid on {order.paidAt}</Message>
+                <Message variant='success'>
+                  Paid on {order.paidAt}
+                </Message>
               ) : (
-                <Message variant='danger'>Not Paid</Message>
+                <Message variant='danger'>
+                  Not Paid
+                </Message>
               )}
             </ListGroup.Item>
 
             <ListGroup.Item>
               <h2>Order Items</h2>
+
               {order.orderItems.length === 0 ? (
-                <Message>Order is empty</Message>
+                <Message>
+                  Order is empty
+                </Message>
               ) : (
                 <ListGroup variant='flush'>
                   {order.orderItems.map((item, index) => (
                     <ListGroup.Item key={index}>
                       <Row>
+
                         <Col md={1}>
                           <Image
                             src={item.image}
@@ -149,55 +168,70 @@ const OrderScreen = ({ match, history }) => {
                             rounded
                           />
                         </Col>
+
                         <Col>
                           <Link to={`/product/${item.product}`}>
                             {item.name}
                           </Link>
                         </Col>
+
                         <Col md={4}>
-                          {item.qty} x ${item.price} = ${item.qty * item.price}
+                          {item.qty} x ${item.price} = $
+                          {item.qty * item.price}
                         </Col>
+
                       </Row>
                     </ListGroup.Item>
                   ))}
                 </ListGroup>
               )}
             </ListGroup.Item>
+
           </ListGroup>
         </Col>
+
         <Col md={4}>
           <Card>
+
             <ListGroup variant='flush'>
+
               <ListGroup.Item>
                 <h2>Order Summary</h2>
               </ListGroup.Item>
+
               <ListGroup.Item>
                 <Row>
                   <Col>Items</Col>
                   <Col>${order.itemsPrice}</Col>
                 </Row>
               </ListGroup.Item>
+
               <ListGroup.Item>
                 <Row>
                   <Col>Shipping</Col>
                   <Col>${order.shippingPrice}</Col>
                 </Row>
               </ListGroup.Item>
+
               <ListGroup.Item>
                 <Row>
                   <Col>Tax</Col>
                   <Col>${order.taxPrice}</Col>
                 </Row>
               </ListGroup.Item>
+
               <ListGroup.Item>
                 <Row>
                   <Col>Total</Col>
                   <Col>${order.totalPrice}</Col>
                 </Row>
               </ListGroup.Item>
+
               {!order.isPaid && (
                 <ListGroup.Item>
+
                   {loadingPay && <Loader />}
+
                   {!sdkReady ? (
                     <Loader />
                   ) : (
@@ -206,9 +240,12 @@ const OrderScreen = ({ match, history }) => {
                       onSuccess={successPaymentHandler}
                     />
                   )}
+
                 </ListGroup.Item>
               )}
+
               {loadingDeliver && <Loader />}
+
               {userInfo &&
                 userInfo.isAdmin &&
                 order.isPaid &&
@@ -223,7 +260,9 @@ const OrderScreen = ({ match, history }) => {
                     </Button>
                   </ListGroup.Item>
                 )}
+
             </ListGroup>
+
           </Card>
         </Col>
       </Row>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -34,14 +34,17 @@ const ProductScreen = ({ history, match }) => {
 
   useEffect(() => {
     if (successProductReview) {
+      alert('Review Submitted')
       setRating(0)
       setComment('')
-    }
-    if (!product._id || product._id !== match.params.id) {
-      dispatch(listProductDetails(match.params.id))
       dispatch({ type: PRODUCT_CREATE_REVIEW_RESET })
     }
-  }, [dispatch, match, successProductReview])
+
+    if (!product._id || product._id !== match.params.id) {
+      dispatch(listProductDetails(match.params.id))
+    }
+
+  }, [dispatch, match.params.id, successProductReview, product._id])
 
   const addToCartHandler = () => {
     history.push(`/cart/${match.params.id}?qty=${qty}`)
@@ -62,6 +65,7 @@ const ProductScreen = ({ history, match }) => {
       <Link className='btn btn-light my-3' to='/'>
         Go Back
       </Link>
+
       {loading ? (
         <Loader />
       ) : error ? (
@@ -69,30 +73,39 @@ const ProductScreen = ({ history, match }) => {
       ) : (
         <>
           <Meta title={product.name} />
+
           <Row>
             <Col md={6}>
               <Image src={product.image} alt={product.name} fluid />
             </Col>
+
             <Col md={3}>
               <ListGroup variant='flush'>
                 <ListGroup.Item>
                   <h3>{product.name}</h3>
                 </ListGroup.Item>
+
                 <ListGroup.Item>
                   <Rating
                     value={product.rating}
                     text={`${product.numReviews} reviews`}
                   />
                 </ListGroup.Item>
-                <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+
+                <ListGroup.Item>
+                  Price: ${product.price}
+                </ListGroup.Item>
+
                 <ListGroup.Item>
                   Description: {product.description}
                 </ListGroup.Item>
               </ListGroup>
             </Col>
+
             <Col md={3}>
               <Card>
                 <ListGroup variant='flush'>
+
                   <ListGroup.Item>
                     <Row>
                       <Col>Price:</Col>
@@ -121,13 +134,11 @@ const ProductScreen = ({ history, match }) => {
                             value={qty}
                             onChange={(e) => setQty(e.target.value)}
                           >
-                            {[...Array(product.countInStock).keys()].map(
-                              (x) => (
-                                <option key={x + 1} value={x + 1}>
-                                  {x + 1}
-                                </option>
-                              )
-                            )}
+                            {[...Array(product.countInStock).keys()].map((x) => (
+                              <option key={x + 1} value={x + 1}>
+                                {x + 1}
+                              </option>
+                            ))}
                           </Form.Control>
                         </Col>
                       </Row>
@@ -144,15 +155,22 @@ const ProductScreen = ({ history, match }) => {
                       Add To Cart
                     </Button>
                   </ListGroup.Item>
+
                 </ListGroup>
               </Card>
             </Col>
           </Row>
+
           <Row>
             <Col md={6}>
               <h2>Reviews</h2>
-              {product.reviews.length === 0 && <Message>No Reviews</Message>}
+
+              {product.reviews.length === 0 && (
+                <Message>No Reviews</Message>
+              )}
+
               <ListGroup variant='flush'>
+
                 {product.reviews.map((review) => (
                   <ListGroup.Item key={review._id}>
                     <strong>{review.name}</strong>
@@ -161,19 +179,27 @@ const ProductScreen = ({ history, match }) => {
                     <p>{review.comment}</p>
                   </ListGroup.Item>
                 ))}
+
                 <ListGroup.Item>
                   <h2>Write a Customer Review</h2>
+
                   {successProductReview && (
                     <Message variant='success'>
                       Review submitted successfully
                     </Message>
                   )}
+
                   {loadingProductReview && <Loader />}
+
                   {errorProductReview && (
-                    <Message variant='danger'>{errorProductReview}</Message>
+                    <Message variant='danger'>
+                      {errorProductReview}
+                    </Message>
                   )}
+
                   {userInfo ? (
                     <Form onSubmit={submitHandler}>
+
                       <Form.Group controlId='rating'>
                         <Form.Label>Rating</Form.Label>
                         <Form.Control
@@ -189,15 +215,17 @@ const ProductScreen = ({ history, match }) => {
                           <option value='5'>5 - Excellent</option>
                         </Form.Control>
                       </Form.Group>
+
                       <Form.Group controlId='comment'>
                         <Form.Label>Comment</Form.Label>
                         <Form.Control
                           as='textarea'
-                          row='3'
+                          rows='3'
                           value={comment}
                           onChange={(e) => setComment(e.target.value)}
-                        ></Form.Control>
+                        />
                       </Form.Group>
+
                       <Button
                         disabled={loadingProductReview}
                         type='submit'
@@ -205,13 +233,16 @@ const ProductScreen = ({ history, match }) => {
                       >
                         Submit
                       </Button>
+
                     </Form>
                   ) : (
                     <Message>
-                      Please <Link to='/login'>sign in</Link> to write a review{' '}
+                      Please <Link to='/login'>sign in</Link> to write a review
                     </Message>
                   )}
+
                 </ListGroup.Item>
+
               </ListGroup>
             </Col>
           </Row>
